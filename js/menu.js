a76116ba0508bd63cbf698d69e5b571bf7bd9d70
CristianInBits <cl.sindila@gmail.com>
2023-09-15 13:59:52 +0200

    JS_menu

---
+++ js/menu.js
@@ -0,0 +1,19 @@
+const iconomenu = document.querySelector(".iconomenu");
+const menu = document.querySelector(".menu-navegacion");
+
+console.log(menu);
+console.log(iconomenu);
+
+iconomenu.addEventListener("click", () => {
+  menu.classList.toggle("spread");
+});
+
+window.addEventListener("click", (e) => {
+  if (
+    menu.classList.contains("spread") &&
+    e.target != menu &&
+    e.target != iconomenu
+  ) {
+    menu.classList.toggle("spread");
+  }
+});
